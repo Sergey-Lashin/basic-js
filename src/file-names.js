69649ed550a41353850c5e0f8af033caf58255cd
Sergey-Lashin <heavyra1n4222@gmail.com>
2024-12-27 14:34:57 +0500

    feat: implement file-names and get-email-domain tasks

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const usedNames = new Map();
+  const result = [];
+
+  for (let name of names) {
+    if (!usedNames.has(name)) {
+      usedNames.set(name, 1);
+      result.push(name);
+    } else {
+      let count = usedNames.get(name);
+      let newName = `${name}(${count})`;
+
+      while (usedNames.has(newName)) {
+        count++;
+        newName = `${name}(${count})`;
+      }
+      usedNames.set(name, count + 1);
+      usedNames.set(newName, 1);
+      result.push(newName);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
